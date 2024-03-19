8afa4ab32813636282ef11e2d4f28f0393a69624
benalfred <59826097+benalfred@users.noreply.github.com>
2024-03-19 07:24:10 +0100

    promo-code integration

--- src/promo-codes/schemas/promo-code.schema.ts
+++ src/promo-codes/schemas/promo-code.schema.ts
@@ -1,5 +1,7 @@
 import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Document, Types} from "mongoose";
 
+export type PROMO_CODE_DOCUMENT = PromoCode & Document;
 @Schema({timestamps:true})
 export class PromoCode {
     @Prop({required:true})
@@ -8,8 +10,14 @@ export class PromoCode {
     @Prop({required:false})
     description:string
 
-    @Prop({required:true})
+    @Prop({required:true, unique:true})
     code:string
+
+    @Prop({required:true})
+    discount:number
+
+    @Prop({required:true, default:0})
+    promo_code_usage_count:number
 
     @Prop({required:true})
     expire_at:string
