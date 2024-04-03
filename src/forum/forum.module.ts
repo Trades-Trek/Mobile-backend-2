b3ee03bc5aaf5256661f5cdb640295968e5cb313
benalfred <59826097+benalfred@users.noreply.github.com>
2024-04-03 08:35:07 +0100

    improvements

--- src/forum/forum.module.ts
+++ src/forum/forum.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ForumService } from './forum.service';
 import { ForumController } from './forum.controller';
+import {Forum, ForumSchema} from "./schemas/forum.schema";
 
 @Module({
+  imports:[{name:Forum.name, schema:ForumSchema}, {name:Chat.name, schema:ChatSchema}],
   controllers: [ForumController],
   providers: [ForumService],
 })
---
+++ src/forum/schemas/forum.schema.ts
@@ -0,0 +1,17 @@
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Types} from "mongoose";
+
+@Schema({timestamps:true})
+export class Forum {
+    @Prop()
+    topic:string
+
+    @Prop({required: true, type: Types.ObjectId,
+        ref: 'User'})
+    creator: Types.ObjectId
+
+    @Prop({required: true, type: Types.ObjectId,
+        ref: 'Competition'})
+    competition: Types.ObjectId
+}
+export const ForumSchema = SchemaFactory.createForClass(Forum)
