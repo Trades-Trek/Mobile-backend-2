47f479939f2dc028d15d81ea1bec5f13a6c5ea87
benalfred <59826097+benalfred@users.noreply.github.com>
2024-05-23 14:22:59 +0100

    improvements

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Post} from '@nestjs/common';
+import {AppService} from './app.service';
 import {ConfigService} from "@nestjs/config";
-import {successResponse} from "./utils/response";
+import {returnErrorResponse, successResponse} from "./utils/response";
 import configuration from "./config/configuration";
 import client from "./config/client";
 import {Public} from "./decorators/public-endpoint.decorator";
+import {useEncryptionService} from "./services/aes-encrypt";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private configService:ConfigService) {}
+    constructor(private readonly appService: AppService, private configService: ConfigService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Public()
-  @Get('/config')
-  getConfig(){
-    return successResponse(client())
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Public()
+    @Get('/config')
+    getConfig() {
+        return successResponse(client())
+    }
+
+    @Public()
+    @Post('/test')
+    test() {
+        try {
+            let password = 'password'
+            password = useEncryptionService().encryptData(password, this.configService.get('DATA_ENCRYPTION_KEY'))
+            console.log(password)
+            const decryptedPassword = useEncryptionService().decryptData(password, this.configService.get('DATA_ENCRYPTION_KEY'))
+            console.log(decryptedPassword)
+        return successResponse('done')
+        } catch (e) {
+            console.log(e)
+            returnErrorResponse(e)
+        }
+    }
 
 }
