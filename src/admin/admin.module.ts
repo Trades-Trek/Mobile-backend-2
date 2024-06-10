1e21609383cce587069e086bd1d22f8c2c7c200c
benalfred <59826097+benalfred@users.noreply.github.com>
2024-06-10 10:45:17 +0100

    Update admin.module.ts

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { AdminService } from './services/admin.service';
 import { AdminController } from './controllers/admin.controller';
 import {AuthModule} from "../auth/auth.module";
 import {WatchlistModule} from "../watchlist/watchlist.module";
+import {AdminUserController} from "./controllers/admin-user.controller";
+import {AdminPlanController} from "./controllers/admin-plan.controller";
 
 @Module({
   imports:[AuthModule, WatchlistModule],
-  controllers: [AdminController],
+  controllers: [AdminController, AdminUserController, AdminPlanController],
   providers: [AdminService],
 })
 export class AdminModule {}
